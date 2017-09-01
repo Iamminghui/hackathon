93d96534a543cc06c9bdfa164b302aa6d8eb27fd
Liu Minghui <minghui@LiumatoMacBook-Pro.local>
2017-09-01 10:06:15 +0200

    [hackathon]: last push

--- src/humanDetection.py
+++ src/humanDetection.py
@@ -2,17 +2,42 @@ import numpy as np
 import imutils
 import cv2
 import time
-import threading
+from threading import Thread
+from threading import Event
 import math
 
-def alarm_info():
-    print "Alert! target is not moving"
+class MyThread(Thread):
 
-def alarm(switch):
-    if switch == "on":
-        timer.start()
-    else: # switch == "off"
-        timer.cancel()
+    period = 0
+    isMoving = True
+    pulse = 0
+    
+    def __init__(self, event):
+        Thread.__init__(self)
+        self.stopped = event
+
+    def update(isMoving, pulse, not_moving_period):
+        self.isMoving = isMoving
+        self.pulse = pulse
+        self.period = not_moving_period
+        
+    def run(self):
+        while not self.stopped.wait(2):
+            print "Target status: isMoving %s pulse %s period %s" \
+                % (str(self.isMoving), str(self.pulse), str(self.period))
+
+def status_on():
+    stopFlag = Event()
+    thread = MyThread(stopFlag)
+    thread.start()
+
+    return thread, stopFlag
+
+def status_off(stopEvent):
+    stopEvent.set()
+
+def status_update(thread, isMoving, pulse, not_moving_period):
+    thread.update(isMoving, pulse, not_moving_period)
         
 def detectWantedPerson(name):
 
@@ -72,12 +97,13 @@ def detectWantedPerson(name):
     
     return ret
 
-def personEntered():
+def detectPersonStatusWithin(time_in_seconds):
 
-    return True
-
-def isPersonMovingWithin(time_in_seconds):
-
+    start_time = time.time()
+    last_frame = ""
+    moving = "False"
+    pulse = 60
+    
     # load the HOG Descriptor
     hog = cv2.HOGDescriptor()
 
@@ -87,55 +113,55 @@ def isPersonMovingWithin(time_in_seconds):
     # open the video capture
     cap = cv2.VideoCapture(0)
     
-    start_time = time.time()
-
     while True:
         # This loop will last only for time_in_seconds seconds
         
         _,frame=cap.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # frame = cv2.resize(frame, (640, 360), 0, 0, cv2.INTER_CUBIC)
+        # TODO: check resolution
         frame = cv2.resize(frame, (400, 225), 0, 0, cv2.INTER_CUBIC)
         
         # scale: which controls by how much the image is resized at each layer
-        found,w = hog.detectMultiScale(frame, winStride=(4,4), padding=(32,32), scale=1.05)
+        found,w = hog.detectMultiScale(frame, winStride=(4,4), padding=(32,32), scale=1.1)
         
         if not w is None:
             # person detected
             draw_detections(frame,found)
-            if isMoving(found):
-                cv2.imshow('feed',frame)
-            else:
-                moving = False
+            cv2.imshow('feed',frame)
 
-            g_last_frame = found
+            if isMoving(found, last_frame):
+                moving = True
+                print "target is moving! detection done!"
+                break
+            
+            last_frame = found
+        else:
+            print "no person detected!!"
             
         # check timeout
         if time.time() - start_time >= time_in_seconds:
-            print "time out. target moved within %s seconds" % (time_in_seconds)  
-            moving = True
+            print "time out. target not moved within %s seconds" % (time_in_seconds)  
             break
 
-        if cv2.waitKey(1) == 27:
-            #for debug purpose
-            moving = True
-            break
-        
-    cv2.destroyAllWindows()
+    cv2.destroyWindow("feed")
+    cap.release()
+
+    return moving, pulse
 
 def inside(r, q):
     rx, ry, rw, rh = r
     qx, qy, qw, qh = q
     return rx > qx and ry > qy and rx + rw < qx + qw and ry + rh < qy + qh
 
-def isMoving(current_img):
+def isMoving(current_img, last_img):
     tolerance = 5 # 5 pixels tolerance
     for xc, yc, wc, hc in current_img:
-        for xl, yl, wl, hl in g_last_frame:
-            print "here 3"
+        for xl, yl, wl, hl in last_img:
             if math.fabs(xc - xl) > tolerance or math.fabs(yc - yl) > tolerance:
+                print "is moving"
                 return True
             else:
+                print "is NOT moving"
                 return False
             
 def draw_detections(img, rects, thickness = 1):
@@ -146,34 +172,37 @@ def draw_detections(img, rects, thickness = 1):
         cv2.rectangle(img, (x+pad_w, y+pad_h), (x+w-pad_w, y+h-pad_h), (0, 255, 0), thickness)
 
 
-
 if __name__ == '__main__':
 
     detected = False
     person = "Lars"
     
-    timer = threading.Timer(1.0, alarm_info)
-
-    g_last_frame = ""
     g_timer_status = False
+    g_timer_thread = ""
+    g_timer_stop_event = ""
     
     while True:
         if not detected:
             # Now someone is entering the apartment
             detected = detectWantedPerson(person)
-            
+
         if detected:
-            # We detected the correct person, keep tracking if he is moving
-            while True:
-                if not isPersonMovingWithin(5):
-                    if not g_timer_status:
-                        # No moving for a person, alarm!!
-                        alarm("on")
-                        g_timer_status = True
-                else: # isPersonMovingWithin(5)
-                    # target is moving
-                    if g_timer_status:
-                        alarm("off")
-                        g_timer_status = False
-        
-                    
+            # start a thread that keep updating the target status
+            # g_timer_thread, g_timer_stop_event = status_on()
+
+            try:
+                # We detected the correct person, keep tracking if he is moving
+                while True:
+                    isMoving, Pulse = detectPersonStatusWithin(30)
+                    #status_update(g_timer_thread, isMoving, Pulse, 30)
+                    print "sleeping!!"
+                    time.sleep(5)
+                    print "Waking UP!!"
+                    if cv2.waitKey(1) == 27:
+                        break
+
+            except (RuntimeError, TypeError, NameError):
+                #status_off(g_timer_stop_event)
+                pass
+            
+            #status_off(g_timer_stop_event)            
